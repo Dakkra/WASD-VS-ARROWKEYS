e5fad441d9f57f6f3161adccd8da6e5aeecd38b4
dakkra <contact@dakkra.com>
2017-04-06 11:01:58 -0600

    Basic animation

--- www/engine.js
+++ www/engine.js
@@ -1,5 +1,28 @@
 var canvas = document.getElementById('canvas');
 var graphics = canvas.getContext("2d");
 
-graphics.fillStyle = "#FFFFFF";
-graphics.fillRect(0, 0, 500, 500);
+function draw() {
+  //Begind procedural composition
+  //Clear the context
+  graphics.clearRect(0, 0, 500, 500);
+  //Paint black background
+  graphics.fillStyle = "#000000";
+  graphics.fillRect(0, 0, 500, 500);
+  //Save default state for fallback
+  graphics.save();
+  //Animation stuff
+  time = new Date();
+  // graphics.rotate(-90 * (Math.PI/180));
+  graphics.strokeStyle = "#FF0000";
+  graphics.lineWidth = 5;
+  graphics.beginPath();
+  graphics.arc(250, 250, 100, 0, (2*Math.PI) * (time.getMilliseconds()/1000));
+  graphics.stroke();
+  // graphics.rotate(90 * (Math.PI/180));
+
+  //Call update to render
+  window.requestAnimationFrame(draw);
+}
+
+//Start animation
+window.requestAnimationFrame(draw);

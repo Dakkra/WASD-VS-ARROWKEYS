ef90e35836c55a492eb8d39e0d3f5a5f45e25921
dakkra <contact@dakkra.com>
2017-04-06 17:53:40 -0600

    Implemented changing of renderers

--- www/engine.js
+++ www/engine.js
@@ -1,26 +1,53 @@
 var canvas = document.getElementById('canvas');
 var graphics = canvas.getContext("2d");
+var testTimer = window.setTimeout(changeRenderer, 1000 * 5);
+var drawFunction = loadingRenderer;
 
-function draw() {
-  //Begind procedural composition
-  //Clear the context
-  graphics.clearRect(0, 0, 500, 500);
-  //Paint black background
-  graphics.fillStyle = "#000000";
-  graphics.fillRect(0, 0, 500, 500);
-  //Save default state for fallback
-  graphics.save();
-  //Animation stuff
-  time = new Date();
-  graphics.strokeStyle = "#888888";
-  graphics.lineWidth = 1;
-  graphics.beginPath();
-  graphics.arc(250, 250, 100, (.5*Math.PI), (2*Math.PI) * (time.getMilliseconds()/1000));
-  graphics.stroke();
+function loadingRenderer() {
+    //Begin procedural composition
+    //Clear the context
+    graphics.clearRect(0, 0, 500, 500);
+    //Paint black background
+    graphics.fillStyle = "#000000";
+    graphics.fillRect(0, 0, 500, 500);
+    //Save default state for fallback
+    graphics.save();
+    //Animation stuff
+    time = new Date();
+    graphics.strokeStyle = "#888888";
+    graphics.lineWidth = 1;
+    graphics.beginPath();
+    graphics.arc(250, 250, 100, (.5 * Math.PI), (2 * Math.PI) * (time.getMilliseconds() / 1000));
+    graphics.stroke();
 
-  //Call update to render
-  window.requestAnimationFrame(draw);
+    //Call update to render
+    window.requestAnimationFrame(drawFunction);
+}
+
+function gameRenderer() {
+    //Begin procedural composition
+    //Clear the context
+    graphics.clearRect(0, 0, 500, 500);
+    //Paint black background
+    graphics.fillStyle = "#0a1431";
+    graphics.fillRect(0, 0, 500, 500);
+    //Save default state for fallback
+    graphics.save();
+    //Animation stuff
+    time = new Date();
+    graphics.strokeStyle = "#888888";
+    graphics.lineWidth = 1;
+    graphics.beginPath();
+    graphics.arc(250, 250, 100, 0, (2 * Math.PI));
+    graphics.stroke();
+
+    //Call update to render
+    window.requestAnimationFrame(drawFunction);
+}
+
+function changeRenderer() {
+    drawFunction = gameRenderer;
 }
 
 //Start animation
-window.requestAnimationFrame(draw);
+window.requestAnimationFrame(drawFunction);

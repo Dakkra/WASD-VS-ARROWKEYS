8328a8bf3ca53d21eb2b02cbbdeb250805c7b09f
dakkra <contact@dakkra.com>
2017-04-14 13:53:11 -0600

    whitepace management

--- www/engine.js
+++ www/engine.js
@@ -8,60 +8,60 @@ var ready = false;
 var selectionSoundID = "Select";
 
 function loadingRenderer() {
-    //Begin procedural composition
-    //Clear the context
-    graphics.clearRect(0, 0, 500, 500);
-    //Paint black background
-    graphics.fillStyle = "#000000";
-    graphics.fillRect(0, 0, 500, 500);
-    //Save default state for fallback
-    graphics.save();
-    //Animation stuff
-    var time = new Date();
-    graphics.strokeStyle = "#888888";
-    graphics.lineWidth = 1;
-    graphics.beginPath();
-    graphics.arc(250, 250, 100, (.5 * Math.PI), (2 * Math.PI) * (time.getMilliseconds() / 1000));
-    graphics.stroke();
-    //Draw loading text
-    graphics.fillStyle = "#888888";
-    graphics.font = "30px Arial";
-    graphics.fillText("Loading...", 185, 260);
+  //Begin procedural composition
+  //Clear the context
+  graphics.clearRect(0, 0, 500, 500);
+  //Paint black background
+  graphics.fillStyle = "#000000";
+  graphics.fillRect(0, 0, 500, 500);
+  //Save default state for fallback
+  graphics.save();
+  //Animation stuff
+  var time = new Date();
+  graphics.strokeStyle = "#888888";
+  graphics.lineWidth = 1;
+  graphics.beginPath();
+  graphics.arc(250, 250, 100, (.5 * Math.PI), (2 * Math.PI) * (time.getMilliseconds() / 1000));
+  graphics.stroke();
+  //Draw loading text
+  graphics.fillStyle = "#888888";
+  graphics.font = "30px Arial";
+  graphics.fillText("Loading...", 185, 260);
 
-    //Call update to render
-    window.requestAnimationFrame(drawFunction);
+  //Call update to render
+  window.requestAnimationFrame(drawFunction);
 
-    //Check if ready to display menu
-    if (ready) {
-      drawFunction = menuRenderer;
-    }
+  //Check if ready to display menu
+  if (ready) {
+    drawFunction = menuRenderer;
+  }
 }
 
 function menuRenderer() {
-    //Begin procedural composition
-    //Clear the context
-    graphics.clearRect(0, 0, 500, 500);
-    //Paint black background
-    graphics.fillStyle = "#0a0720";
-    graphics.fillRect(0, 0, 500, 500);
-    //Save default state for fallback
-    graphics.save();
-    //Animation stuff
-    var time = new Date();
-    graphics.strokeStyle = "#888888";
-    graphics.lineWidth = 1;
-    graphics.beginPath();
-    var rad = 100 * (time.getMilliseconds()/1000);
-    graphics.arc(250, 250, rad, 0, (2 * Math.PI));
-    graphics.stroke();
+  //Begin procedural composition
+  //Clear the context
+  graphics.clearRect(0, 0, 500, 500);
+  //Paint black background
+  graphics.fillStyle = "#0a0720";
+  graphics.fillRect(0, 0, 500, 500);
+  //Save default state for fallback
+  graphics.save();
+  //Animation stuff
+  var time = new Date();
+  graphics.strokeStyle = "#888888";
+  graphics.lineWidth = 1;
+  graphics.beginPath();
+  var rad = 100 * (time.getMilliseconds()/1000);
+  graphics.arc(250, 250, rad, 0, (2 * Math.PI));
+  graphics.stroke();
 
-    //Menu text
-    graphics.font = "30px Arial";
-    graphics.fillStyle = "#FFFFFF";
-    graphics.fillText("Play", 220, 260);
+  //Menu text
+  graphics.font = "30px Arial";
+  graphics.fillStyle = "#FFFFFF";
+  graphics.fillText("Play", 220, 260);
 
-    //Call update to render
-    window.requestAnimationFrame(drawFunction);
+  //Call update to render
+  window.requestAnimationFrame(drawFunction);
 }
 
 //keyboard input handle for the menu
